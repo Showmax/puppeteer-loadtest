590c8dbe658342209af55b8711eabfab521b719a
David Viktora <david.viktora@showmax.com>
2020-06-17 18:08:44 +0200

    Make the script really run concurrently

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const argv = require('minimist')(process.argv.slice(2));
 const perf = require('execution-time')();
 
 const file = argv.file;
-const samplesRequested = argv.s || 1;
 const concurrencyRequested = argv.c || 1;
+const totalScenarios = argv.n || 10;
 const silent = argv.silent || false;
 
 if (!file) {
@@ -20,60 +20,39 @@ if (!silent) {
   createDebug.enable('puppeteer-loadtest');
 }
 
-if (!samplesRequested) {
-  debug('no sample is specified, using 1 as default')
-}
 
 if (!concurrencyRequested) {
   debug('no concurrency is specified, using 1 as default')
 }
 
+if (!totalScenarios) {
+  debug('no total scenarios cound is specified, using 10 as default')
+}
+
 debug('puppeteer-loadtest is loading...');
 
 const cmdToExecute = `node ${file}`;
-const results = {};
-let samplesCount = 0;
-let concurrencyCount = 0;
+let totalScenariosStarted = 0;
 
-const executeTheCommand = function(cmd) {
+const executeTheCommand = function() {
+  totalScenariosStarted += 1;
+  console.log(`Scenarios started: ${totalScenariosStarted}`);
   return new Promise((resolve, reject) => {
-    exec(cmd, function(error, stdout, stderr) {
-      concurrencyCount += 1;
-      debug(`sample: ${samplesCount}`);
-      debug(`concurrent: ${concurrencyCount}`);
+    exec(cmdToExecute, function(error, stdout, stderr) {
       if(stderr) reject(stderr);
       if(error) reject(error);
-      resolve(stdout);
+      console.log(stdout);
+      resolve();
     });
-  });   
-};
-
-const doAnotherSample = () => { 
-  if(samplesCount < samplesRequested) {
-    doConcurrency();
-    samplesCount += 1;
-  }
-};
-
-const doConcurrency = () => {
-  const promisesArray = [];
-
-  for(let i=0; i<concurrencyRequested; i += 1) {
-    promisesArray.push(
-      executeTheCommand(cmdToExecute)
-    );
-  }
-
-  concurrencyCount = 0;
-  Promise.all(promisesArray)
-  .then(values => { 
-    debug(values);
-    doAnotherSample();
   })
-  .catch(error => {
-    debug(error);
-    doAnotherSample();
-  });
+    .then(function () {
+      if (totalScenariosStarted < totalScenarios){
+        return executeTheCommand();
+      }
+  })
 };
 
-doAnotherSample();
+let promiseList = [];
+for(let i=0; i<concurrencyRequested; i += 1) {
+  promiseList.push(executeTheCommand(cmdToExecute));
+}
